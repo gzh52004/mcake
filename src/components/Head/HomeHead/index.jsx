db9594dc22b3e4dbd08f5ad7eca6b87c54078069
mayuan <m2844456583@163.com>
2020-10-26 16:53:43 +0800

    卸载掉antd安装file-loader插件

--- src/components/Head/HomeHead/index.jsx
+++ src/components/Head/HomeHead/index.jsx
@@ -9,15 +9,19 @@ let HomeHead = function (props) {
     return (
         <div className="header">
             <div className="beijing">
+                <span className="iconfont icon-location"></span>
                 <span className="text">北京市</span>
             </div>
             <div className="picture">
                 <img src="./img/cake.png" alt=""/>
             </div>
             <div className="search">
-                <span className="iconfont">&#xe600;</span>
+                <span className="iconfont icon-sousuo"></span>
             </div>
-            <div className="mine"></div>
+            <span className="shuxian">|</span>
+            <div className="mine">
+                <span className="iconfont icon-toggle"></span>
+            </div>
         </div >
     )
 }
